3b1a48b02035b8fc118ca0556e01e228a88aaf8e
Ki_Lop <ilovevy159@gmail.com>
2023-05-18 11:00:24 +0700

    p

--- db/role/role_after_login.sql
+++ db/role/role_after_login.sql
@@ -17,10 +17,19 @@ GRANT INSERT, UPDATE ON budget TO [role_after_login];
 -- STORE PROCEDURE AFTER LOGIN:
 GRANT EXECUTE ON sp_delete_reg_courses TO [role_after_login];
 GRANT EXECUTE ON sp_reg_courses TO [role_after_login];
+GRANT EXECUTE ON sp_create_budget TO [role_after_login];
+GRANT EXECUTE ON sp_deposit_budget TO [role_after_login];
+GRANT EXECUTE ON sp_set_payment_status_to_paid TO [role_after_login];
+GRANT EXECUTE ON sp_deduct_from_budget TO [role_after_login];
+GRANT EXECUTE ON sp_processTransactionProc TO [role_after_login];
+GRANT EXECUTE ON sp_Loop_TransactionForAllOrders TO [role_after_login];
+GRANT EXECUTE ON sp_processTransactionForAllOrders TO [role_after_login];
+
 -- SCALAR VALUED FUNCTIONS:
 GRANT EXECUTE ON func_num_reg_courses TO [role_after_login];
 GRANT EXECUTE ON func_total_money_reg_courses TO [role_after_login];
 GRANT EXECUTE ON func_check_wallet_exist TO [role_after_login];
-
+GRANT EXECUTE ON func_get_current_money TO [role_after_login];
 -- TABLE VALUED FUNCTIONS:
 GRANT SELECT ON func_list_all_reg_courses TO [role_after_login];
+GRANT SELECT ON func_list_all_paid_courses TO [role_after_login];
